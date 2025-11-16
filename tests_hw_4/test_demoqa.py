93df55288f36b3779350f8d4a9768f43f9ad9ed6
Elizaveta Sytaya <sytayaelizaveta2000@mail.ru>
2025-11-16 22:51:15 +0300

    hw_4 done

---
+++ tests_hw_4/test_demoqa.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import pytest
+from selene import browser, by, have, command
+
+
+def test_demoqa():
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.element('#firstName').type('Elizaveta')
+    browser.element('#lastName').type('Sytaya')
+    browser.element('#userEmail').type('eliza@mail.ru')
+    browser.element(by.text('Female')).click()
+    browser.element('#userNumber').type('89031112233')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click().element(by.text('September')).click()
+    browser.element('.react-datepicker__year-select').click().element(by.text('2000')).click()
+    browser.element('.react-datepicker__day--001').click()
+    browser.element('#subjectsInput').type('blablabla')
+    browser.element(by.text('Music')).click()
+
+    browser.element("#uploadPicture").perform(command.js.scroll_into_view).click()
+
+    picture_path = str(Path(__file__).parent.joinpath('resources', 'picture.horse.jpg').resolve())
+    browser.element('#uploadPicture').set_value(picture_path).with_(timeout=browser.config.timeout * 6)
+
+    browser.element('#currentAddress').type('Moscow')
+    browser.element('#state').click()
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#city').click()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
+
+    browser.element('#submit').click()
+
+    browser.element('#example-modal-sizes-title-lg').should(
+        have.text('Thanks for submitting the form')
+    )
+
+    # Таблица с результатами
+    results = browser.element('.table-responsive')
+    results.should(have.text('Elizaveta Sytaya'))
+    results.should(have.text('eliza@mail.ru'))
+    results.should(have.text('Female'))
+    results.should(have.text('9031112233'))
+    results.should(have.text('1 September,2000'))
+    results.should(have.text('blablabla'))  # сайт отображает "Maths"
+    results.should(have.text('Music'))
+    results.should(have.text('Moscow'))
+    results.should(have.text('Haryana Karnal'))
